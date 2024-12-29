b92078296a3fa0ff16ed75be3d8f151794ca2a96
black-sonic4 <mohamad.work.01@gmail.com>
2024-12-29 17:11:17 +0400

    init-commit

---
+++ EmployeesSalary/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace EmployeesSalary
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int BaseSalary { get; set; }
+        public string Type { get; set; }
+        public DateOnly JoinDate {  get; set; }
+        public int CalculateSalary()
+        {
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+
+            int totalMonths = ((today.Year - JoinDate.Year) * 12) + (today.Month - JoinDate.Month);
+            double rate = 0.0;
+            if (Type == "Manager")
+            {
+                if (totalMonths >= 12)
+                    rate = 0.10;
+                if (totalMonths >= 16)
+                    rate = 0.25;
+            }
+            if (Type == "Salesman")
+            {
+                if (totalMonths >= 6)
+                    rate = 0.01;
+            }
+            if (Type == "Secretary")
+            {
+                rate = 0.05;
+            }
+            return (int) Math.Floor(BaseSalary + rate * BaseSalary);
+        }
+
+    }
+}
--- EmployeesSalary/Program.cs
+++ EmployeesSalary/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeesSalary;
+
+Employee manager = new Employee()
+{
+    Name = "Ahmad",
+    JoinDate = new DateOnly(2024, 1, 1),
+    Type = "Manager",
+    BaseSalary = 20000,
+};
+
+
+Employee salesman = new Employee()
+{
+    Name = "Mohamad",
+    JoinDate = new DateOnly(2024, 10, 1),
+    Type = "Salesman",
+    BaseSalary = 10000,
+};
+
+Employee secretary = new Employee()
+{
+    Name = "Rami",
+    JoinDate = new DateOnly(2024, 10, 1),
+    Type = "Secretary",
+    BaseSalary = 5000,
+};
+
+foreach (Employee employee in new[] { manager, salesman, secretary }) {
+    Console.WriteLine("Employee " + employee.Name + "/" + employee.Type + ". Joined in: " + employee.JoinDate + " BaseSalary: " + employee.BaseSalary + ". Total Salary: " + employee.CalculateSalary());
+}
+
+manager.JoinDate = new DateOnly(2023, 10, 1);
+salesman.JoinDate = new DateOnly(2024, 1, 1);
+secretary.JoinDate = new DateOnly(2020, 1, 1);
+
+Console.WriteLine("Afeter editing - 1:");
+
+foreach (Employee employee in new[] { manager, salesman, secretary })
+{
+    Console.WriteLine("Employee " + employee.Name + "/" + employee.Type + ". Joined in: " + employee.JoinDate + " BaseSalary: " + employee.BaseSalary + ". Total Salary: " + employee.CalculateSalary());
+}
+
+manager.JoinDate = new DateOnly(2023, 1, 1);
+salesman.JoinDate = new DateOnly(2024, 1, 1);
+secretary.JoinDate = new DateOnly(2020, 1, 1);
+
+Console.WriteLine("Afeter editing - 2:");
+
+foreach (Employee employee in new[] { manager, salesman, secretary })
+{
+    Console.WriteLine("Employee " + employee.Name + "/" + employee.Type + ". Joined in: " + employee.JoinDate + " BaseSalary: " + employee.BaseSalary + ". Total Salary: " + employee.CalculateSalary());
+}
